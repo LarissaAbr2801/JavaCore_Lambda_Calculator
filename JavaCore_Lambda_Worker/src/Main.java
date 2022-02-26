12b897ffe7b5b74cc8b40dc6276bb64e6fc96aba
LarissaAbr2801 <97035851+LarissaAbr2801@users.noreply.github.com>
2022-02-26 23:34:16 +0300

    Delete Main.java

--- JavaCore_Lambda_Worker/src/Main.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-
-    public static void main(String[] args) {
-        OnTaskDoneListener listener = System.out::println;
-        OnTaskErrorListener listener33 = () -> System.out.println("Error!");
-        Worker worker = new Worker(listener, listener33);
-        worker.start();
-    }
-}
